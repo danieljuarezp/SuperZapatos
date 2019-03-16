d4deb8ddaa65a12f17276488919cebee465aee8b
Daniel Juárez <danieljuarezp@hotmail.com>
2019-03-16 16:50:19 -0700

    Se cambio respuesta de servicio

--- SuperZapatos/Controllers/ServiceController.cs
+++ SuperZapatos/Controllers/ServiceController.cs
@@ -8,6 +8,7 @@ using System.Web.Http;
 
 namespace SuperZapatos.Controllers
 {
+    // [Authorize] sirve para solo servir a usuarios autenticados
     public class ServiceController : ApiController
     {
         private DBContext db;
@@ -21,16 +22,20 @@ namespace SuperZapatos.Controllers
         [HttpGet]
         public IHttpActionResult GetAllStores()
         {
-            var algo = new List<string>();
-            return Ok(new { artiles = algo });
+            bool success = false;
+            var stores = db.Stores;
+            success = true;
+            return Ok(new { success, stores });
         }
 
         [Route("Services/Articles")]
         [HttpGet]
         public IHttpActionResult GetAllArticles()
         {
-            var algo = new List<string>();
-            return Ok(new { artiles = algo });
+            bool success = false;
+            var articles = db.Articles;
+            success = true;
+            return Ok(new { success, articles });
         }
 
         [Route("Services/Articles/Stores/{id}")]
@@ -38,24 +43,15 @@ namespace SuperZapatos.Controllers
         public IHttpActionResult GetAllArticlesByStore(int id)
         {
             bool success = false;
-
             if (!db.Stores.Any(q => q.Id == id))
             {
                 var error_code = HttpStatusCode.NotFound;
                 var error_msg = "Record not Found";
                 return Ok(new { success, error_code, error_msg });
             }
-
-            if (id == 0)
-            {
-                var error_code = HttpStatusCode.BadRequest;
-                var error_msg = "malo";
-                return Ok(new { success, error_code, error_msg });
-            }
-
+            success = true;
             var allArticles = db.Articles.Where(q => q.StoreId == id);
-            var algo = new List<string>() { "1" };
-            return Ok(new { success, artiles = algo, also = "" });
+            return Ok(new { success, allArticles });
         }
     }
 }
